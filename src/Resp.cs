e1d4707a511cdaf77233e09980f938bb3515d1d3
DarKrone <Brener-2000@mail.ru>
2024-11-06 22:10:48 +0400

    ParseCommands

--- src/Resp.cs
+++ src/Resp.cs
@@ -11,14 +11,14 @@ namespace codecrafters_redis.src
         public static string[] ParseMessage(string data)
         {
             List<string> result = new List<string>();
-            Console.WriteLine(@data);
-            int n = int.Parse(data.Substring(1, data.IndexOf(@"\r\n") - 1));
+            //Console.WriteLine(data);
+            int n = int.Parse(data.Substring(1, data.IndexOf("\r\n") - 1));
 
             //Console.WriteLine(n);
-            data = data.Substring(data.IndexOf(@"\r\n") + 4);
+            data = data.Substring(data.IndexOf("\r\n") + 2);
             //Console.WriteLine(data);
 
-            for(int i = 0 ; i < n; i++)
+            for (int i = 0 ; i < n; i++)
             {
                 result.Add(ParseBulkString(ref data));
             }
@@ -27,11 +27,12 @@ namespace codecrafters_redis.src
 
         private static string ParseBulkString(ref string data)
         {
-            int n = int.Parse(data.Substring(1, data.IndexOf("\\r\\n") - 1));
-            data = data.Substring(data.IndexOf(@"\r\n") + 4);
+            Console.WriteLine(data);
+            int n = int.Parse(data.Substring(1, data.IndexOf("\r\n") - 1));
+            data = data.Substring(data.IndexOf("\r\n") + 2);
             //Console.WriteLine(data);
             string text = data.Substring(0, n);
-            data = data.Substring(data.IndexOf(@"\r\n") + 4);
+            data = data.Substring(data.IndexOf("\r\n") + 2);
             //Console.WriteLine(data);
 
             return text;
