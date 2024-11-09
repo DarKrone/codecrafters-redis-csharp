40cbf34057415621efe08037f31048977ca8266c
DarKrone <Brener-2000@mail.ru>
2024-11-09 20:18:03 +0400

    TestDbRead

--- src/Rdb.cs
+++ src/Rdb.cs
@@ -26,11 +26,13 @@ namespace codecrafters_redis.src
         public async void ReadDb()
         {
             using (StreamReader reader = new StreamReader($"{config["dir"]}/{config["dbfilename"]}"))
-            {
+            { //52-45-44-49-53-30-30-31-31-EF-BF-BD-09-72-65-64-69-73-2D-76-65-72-05-37-2E-32-2E-30-EF-BF-BD-0A-72-65-64-69-73-2D-62-69-74-73-EF-BF-BD-40-EF-BF-BD-00-EF-BF-BD-01-00-00-04-70-65-61-72-09-62-6C-75-65-62-65-72-72-79-EF-BF-BD-EF-BF-BD-4A-D0-90-79-EF-BF-BD-EF-BF-BD-0A
                 string text = await reader.ReadToEndAsync();
                 byte[] bstring = Encoding.UTF8.GetBytes(text);
-                string hexString = BitConverter.ToString(bstring);
-                Console.WriteLine(hexString);
+                foreach (byte b in bstring)
+                {
+                    Console.Write(b + "-");
+                }
             }
         }
     }
