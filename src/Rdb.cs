249cf3d12e1e1bb99cf87edb27de02880e6e9217
DarKrone <Brener-2000@mail.ru>
2024-11-10 18:14:49 +0400

    TestDbRead

--- src/Rdb.cs
+++ src/Rdb.cs
@@ -50,28 +50,45 @@ namespace codecrafters_redis.src
                 switch (indicator)
                 {
                     case "00":
+                        Console.WriteLine(dbHexString);
                         ReadKeyValue(ref dbHexString);
+                        Console.WriteLine(dbHexString);
                         break;
                     case "FC":
                         {
+                            Console.WriteLine(dbHexString);
                             dbHexString = dbHexString[2..];
                             int milliseconds = Int32.Parse(dbHexString.Substring(0, 8), System.Globalization.NumberStyles.HexNumber);
                             dbHexString = dbHexString[8..];
                             ReadKeyValue(ref dbHexString, milliseconds);
+                            Console.WriteLine(dbHexString);
                             break;
                         }
                     case "FD":
                         {
+                            Console.WriteLine(dbHexString);
                             dbHexString = dbHexString[2..];
                             int milliseconds = Int32.Parse(dbHexString.Substring(0, 4), System.Globalization.NumberStyles.HexNumber);
                             dbHexString = dbHexString[4..];
                             ReadKeyValue(ref dbHexString, milliseconds);
+                            Console.WriteLine(dbHexString);
                             break;
                         }
                 }
             }
         }
-
+        //52-45-44-49-53-30-30-31-31-
+        //FA-
+        //09-72-65-64-69-73-2D-76-65-72-05-37-2E-32-2E-30-
+        //FA-0A-72-65-64-69-73-2D-62-69-74-73-C0-40-FE-00-
+        //
+        //FB-
+        //05-
+        //05-
+        //FC-
+        //00-
+        //0C-28-8A-C7-01-00-00-00-06-62-61-6E-61-
+        //6E-61-09-72-61-73-70-62-65-72-72-79-FC-00-0C-28-8A-C7-01-00-00-00-06-6F-72-61-6E-67-65-09-70-69-6E-65-61-70-70-6C-65-FC-00-0C-28-8A-C7-01-00-00-00-05-67-72-61-70-65-06-62-61-6E-61-6E-61-FC-00-9C-EF-12-7E-01-00-00-00-05-61-70-70-6C-65-0A-73-74-72-61-77-62-65-72-72-79-FC-00-0C-28-8A-C7-01-00-00-00-04-70-65-61-72-05-6D-61-6E-67-6F-FF-E8-32-4B-92-7F-92-89-7C-0A
         private void ReadKeyValue(ref string dbHexString, int milliseconds = -1)
         {
             dbHexString = dbHexString[2..];
